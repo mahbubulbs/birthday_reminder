837f9209b5cf05ea8952c34ad86df98ff624023d
Md Mahbubul Hasan <mahbubul.hasan@brainstation-23.com>
2026-06-07 16:54:47 +0600

    [ADD] birthday_reminder: add manifest file

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Birthday Reminder",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Md Mahbubul Hasan",
+    "website": "https://erp23.com/",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/birthday_views.xml",
+    ],
+    "application": True,
+}
